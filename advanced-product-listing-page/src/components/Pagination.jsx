f722f05ffdfc4e4c271ea4ed2a09585db6e133ff
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-01-22 13:11:35 +0530

    Final Commit for Pagination

--- advanced-product-listing-page/src/components/Pagination.jsx
+++ advanced-product-listing-page/src/components/Pagination.jsx
@@ -1,7 +1,23 @@
-const Pagination = () => {
-    return ( 
-        <div></div>
-     );
-}
- 
+const Pagination = ({ totalPages, currentPage, onPageChange }) => {
+  return (
+    <div className="flex justify-center gap-2 mt-8">
+      {Array.from({ length: totalPages }, (_, index) => {
+        const page = index + 1;
+
+        return (
+          <button
+            key={page}
+            onClick={() => onPageChange(page)}
+            className={`px-4 py-2 rounded border ${
+              page === currentPage ? "bg-black text-white" : "bg-white"
+            }`}
+          >
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
 export default Pagination;
--- advanced-product-listing-page/src/components/ProductList.jsx
+++ advanced-product-listing-page/src/components/ProductList.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import ProductCard from "./ProductCard";
 import Filters from "./Filters";
 import Sort from "./Sort";
+import Pagination from "./Pagination";
 
 const ProductList = ({ products }) => {
   // 1️⃣ State lives in parent
   const [selectedCategory, setSelectedCategory] = useState("all");
   const [sortOrder, setSortOrder] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const productsperPage = 6;
 
   // 2️⃣ Get unique categories from products
   const categories = [...new Set(products.map((product) => product.category))];
@@ -28,7 +32,15 @@ const ProductList = ({ products }) => {
     sortedProducts.sort((a, b) => b.price - a.price);
   }
 
-  // 5️⃣ RENDER
+  // 5️⃣ PAGINATION
+  const startIndex = (currentPage - 1) * productsperPage;
+  const endIndex = startIndex + productsperPage;
+
+  const paginatedProducts = sortedProducts.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(sortedProducts.length / productsperPage);
+
+  // 6 RENDER
   return (
     <div className="p-6">
       {/* Filters + Sort Controls */}
@@ -44,10 +56,17 @@ const ProductList = ({ products }) => {
 
       {/* Product Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {sortedProducts.map((product) => (
+        {paginatedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+
+      {/* PAGINATION */}
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </div>
   );
 };
